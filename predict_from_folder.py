42e9effe17e8c47d44036b94dd5d9177af900d72
William <grusinator@gmail.com>
2017-10-04 17:31:41 +0200

    added zoom

--- predict_from_folder.py
+++ predict_from_folder.py
@@ -180,67 +180,6 @@ def get_trained_model(args):
     return model
 
 
-def forward_pass(args):
-    ''' Runs a forward pass to segment the image. '''
-
-    model = get_trained_model(args)
-
-    # Load image and swap RGB -> BGR to match the trained weights
-    image_rgb = np.array(Image.open(args.input_path)).astype(np.float32)
-    image = image_rgb[:, :, ::-1] - args.mean
-    image_size = image.shape
-
-    # Network input shape (batch_size=1)
-    net_in = np.zeros((1, input_height, input_width, 3), dtype=np.float32)
-
-    output_height = input_height - 2 * label_margin
-    output_width = input_width - 2 * label_margin
-
-    # This simplified prediction code is correct only if the output
-    # size is large enough to cover the input without tiling
-    assert image_size[0] < output_height
-    assert image_size[1] < output_width
-
-    # Center pad the original image by label_margin.
-    # This initial pad adds the context required for the prediction
-    # according to the preprocessing during training.
-    image = np.pad(image,
-                   ((label_margin, label_margin),
-                    (label_margin, label_margin),
-                    (0, 0)), 'reflect')
-
-    # Add the remaining margin to fill the network input width. This
-    # time the image is aligned to the upper left corner though.
-    margins_h = (0, input_height - image.shape[0])
-    margins_w = (0, input_width - image.shape[1])
-    image = np.pad(image,
-                   (margins_h,
-                    margins_w,
-                    (0, 0)), 'reflect')
-
-    # Run inference
-    net_in[0] = image
-    prob = model.predict(net_in)[0]
-
-    # Reshape to 2d here since the networks outputs a flat array per channel
-    prob_edge = np.sqrt(prob.shape[0]).astype(np.int)
-    prob = prob.reshape((prob_edge, prob_edge, 21))
-
-    # Upsample
-    if args.zoom > 1:
-        prob = interp_map(prob, args.zoom, image_size[1], image_size[0])
-
-    # Recover the most likely prediction (actual segment class)
-    prediction = np.argmax(prob, axis=2)
-
-    # Apply the color palette to the segmented image
-    color_image = np.array(pascal_palette)[prediction.ravel()].reshape(
-        prediction.shape + (3,))
-
-    print('Saving results to: ', args.output_path)
-    with open(args.output_path, 'wb') as out_file:
-        Image.fromarray(color_image).save(out_file)
-
 
 def main():
     parser = argparse.ArgumentParser()
